b5331377471ef2e89c51ea54f8871acf3d237d8c
Michael Kilby <kilbyjmichael@gmail.com>
2016-04-25 06:20:12 -0500

    Added js keys

---
+++ start/script.js
@@ -0,0 +1,5 @@
+Mousetrap.bind('a b t v', function(e) { window.location.replace('https://animebytes.tv/')});
+Mousetrap.bind('f', function(e) { window.location.replace('https://facebook.com/')});
+Mousetrap.bind('m m', function(e) { window.location.replace('https://messenger.com/')});
+Mousetrap.bind('g m', function(e) { window.location.replace('https://gmail.com/')});
+Mousetrap.bind('f m', function(e) { window.location.replace('https://gmail.com/')});
