43962c79bd4191eba9318dce83d6bccb477ef5ae
Michael Kilby <kilbyjmichael@gmail.com>
2016-04-26 23:28:02 -0500

    added google search bar

--- start/script.js
+++ start/script.js
@@ -3,3 +3,9 @@ Mousetrap.bind('f', function(e) { window.location.replace('https://facebook.com/
 Mousetrap.bind('m m', function(e) { window.location.replace('https://messenger.com/')});
 Mousetrap.bind('g m', function(e) { window.location.replace('https://gmail.com/')});
 Mousetrap.bind('f m', function(e) { window.location.replace('https://gmail.com/')});
+
+function searchForm(){
+    var query = document.searchForm.q.value;
+    location='http://www.google.com/search?q=' + encodeURIComponent(query);
+    return(location);
+};
