37e9012ae301b1fe54f2b8b80823ae4ae2b58677
Michael Kilby <kilbyjmichael@gmail.com>
2016-04-29 00:34:41 -0500

    added multi search

--- start/script.js
+++ start/script.js
@@ -5,7 +5,23 @@ Mousetrap.bind('g m', function(e) { window.location.replace('https://gmail.com/'
 Mousetrap.bind('f m', function(e) { window.location.replace('https://gmail.com/')});
 
 function searchForm(){
-    var query = document.searchForm.q.value;
-    location='https://www.google.com/search?q=' + encodeURIComponent(query);
-    return(location);
+    var query = document.searchForm.q.value.split(":");
+    if(query.length == 2) { // This means there's a :
+        if(query[0] == 'g') {
+            location = 'https://www.google.com/search?q=' + encodeURIComponent(query[1]);
+            return(location);
+        }
+        if(query[0] == 'd') {
+            location = 'https://duckduckgo.com/?q=' + encodeURIComponent(query[1]);
+            return(location);
+        }
+        if(query[0] == 'a') {
+            location = 'http://myanimelist.net/search/all?q=' + encodeURIComponent(query[1]);
+            return(location);
+        }
+    }
+    else {
+        location = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+        return(location);
+    }
 };
