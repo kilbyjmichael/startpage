763305e8495b81afb4cfeea8a4d2f8b511f0e1eb
Michael Kilby <kilbyjmichael@gmail.com>
2016-05-01 23:51:37 -0500

    added image, js

--- start/script.js
+++ start/script.js
@@ -2,10 +2,11 @@ Mousetrap.bind('a b t v tab', function(e) { window.location.replace('https://ani
 Mousetrap.bind('f a c tab', function(e) { window.location.replace('https://facebook.com/')});
 Mousetrap.bind('m e s tab', function(e) { window.location.replace('https://messenger.com/')});
 Mousetrap.bind('g m tab', function(e) { window.location.replace('https://gmail.com/')});
-Mousetrap.bind('f a tab', function(e) { window.location.replace('https://fastmail.com/')});
+Mousetrap.bind('f m tab', function(e) { window.location.replace('https://fastmail.com/')});
 Mousetrap.bind('r e g tab', function(e) { window.location.replace('https://reggienet.ilstu.edu/')});
 Mousetrap.bind('g i t tab', function(e) { window.location.replace('https://github.com/')});
 Mousetrap.bind('r e d tab', function(e) { window.location.replace('https://reddit.com/')});
+Mousetrap.bind('m a l tab', function(e) { window.location.replace('https://myanimelist.net/animelist/sageinventor')});
 
 
 function searchForm(){
@@ -15,16 +16,20 @@ function searchForm(){
             location = 'https://www.google.com/search?q=' + encodeURIComponent(query[1]);
             return(location);
         }
-        if(query[0] == 'd') {
+        if(query[0] == 'ddg') {
             location = 'https://duckduckgo.com/?q=' + encodeURIComponent(query[1]);
             return(location);
         }
-        if(query[0] == 'a') {
+        if(query[0] == 'mal') {
             location = 'http://myanimelist.net/search/all?q=' + encodeURIComponent(query[1]);
             return(location);
         }
-	    if(query[0] == 'gg') {
+	    if(query[0] == 'gmail') {
 		    location = 'https://mail.google.com/mail/u/0/#search/' + encodeURIComponent(query[1]);
+		    return(location);
+	    }
+	    if(query[0] == 'git') {
+		    location = 'https://github.com/search?utf8=%E2%9C%93&q=' + encodeURIComponent(query[1]);
 		    return(location);
 	    }
     }
